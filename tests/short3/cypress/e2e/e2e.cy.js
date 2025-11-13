9e2362f5c14eb511883b0bc8b01d488463bfd114
realjunderwood <75750795+realjunderwood@users.noreply.github.com>
2025-11-13 00:07:53 -0500

    updated cypress test

--- tests/short3/cypress/e2e/e2e.cy.js
+++ tests/short3/cypress/e2e/e2e.cy.js
@@ -1,20 +1,72 @@
-/* eslint-disable cypress/unsafe-to-chain-command */
+const makeVideo = (id, title) => ({
+  id: { kind: 'youtube#video', videoId: id },
+  snippet: {
+    title,
+    description: `${title} description`,
+    thumbnails: {
+      default: { url: `https://img.youtube.com/vi/${id}/default.jpg` },
+    },
+  },
+});
+
+const defaultVideos = [
+  makeVideo('spooked', 'Spooked And Confused'),
+  makeVideo('daywalk', 'Day Walking Adventures'),
+];
+
+const jazzVideos = [
+  makeVideo('smooth', 'Smooth Jazz Vibes'),
+  makeVideo('late', 'Late Night Sax Sessions'),
+];
+
 describe('Testing for React Intro Lab', () => {
   beforeEach(() => {
-    cy.visit('/'); // may need changing to match route
-    cy.get('input')
-      .type('spooked and confused')
-      .should('have.value', 'spooked and confused');
+    cy.intercept('GET', '**/youtube/v3/search**', (req) => {
+      const term = `${req.query?.q || ''}`.toLowerCase();
+      if (term.includes('jazz')) {
+        req.reply({ statusCode: 200, body: { items: jazzVideos } });
+        return;
+      }
+
+      req.reply({ statusCode: 200, body: { items: defaultVideos } });
+    }).as('youtubeSearch');
+
+    cy.visit('/');
+    cy.wait('@youtubeSearch');
   });
 
-  it('should update video list when typing in the search bar', () => {
+  it('updates video list and detail when searching for a new topic', () => {
+    cy.get('input').clear().type('jazz vibes{enter}');
+    cy.wait('@youtubeSearch');
+
     cy.get('#video-list > li')
+      .should('have.length', jazzVideos.length)
       .first()
-      .should('contain.text', 'Spooked And Confused');
+      .should('contain.text', jazzVideos[0].snippet.title);
+
+    cy.get('#video-detail').should('contain.text', jazzVideos[0].snippet.title);
   });
 
-  it('should update video detail when typing in the search bar', () => {
+  it('changes the focused video when a different list item is chosen', () => {
+    cy.get('#video-list > li').should('have.length', defaultVideos.length);
+    cy.get('#video-detail').should('contain.text', defaultVideos[0].snippet.title);
+
+    cy.get('#video-list > li').eq(1).click();
+
     cy.get('#video-detail')
-      .should('contain.text', 'Spooked And Confused');
+      .should('contain.text', defaultVideos[1].snippet.title)
+      .and('not.contain.text', defaultVideos[0].snippet.title);
+  });
+
+  it('restores the default feed when the search box is cleared and submitted', () => {
+    cy.get('input').clear().type('jazz vibes{enter}');
+    cy.wait('@youtubeSearch');
+    cy.get('#video-detail').should('contain.text', jazzVideos[0].snippet.title);
+
+    cy.get('input').clear().type('{enter}');
+    cy.wait('@youtubeSearch');
+
+    cy.get('#video-list > li').first().should('contain.text', defaultVideos[0].snippet.title);
+    cy.get('#video-detail').should('contain.text', defaultVideos[0].snippet.title);
   });
 });
